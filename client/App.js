5374477454291b6084c40957ba3b804c0d318b20
Sultan Atwal <sultan.singh.atwal@gmail.com>
2021-10-01 19:09:07 -0700

    #7 feat: add bottom tab navigation

--- client/App.js
+++ client/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App() {
+function HomeScreen() {
 	// Fetch documents
 	const fetchBottles = async () => {
 		const url = `http://10.0.2.2:8080/bottles`;
@@ -14,6 +16,16 @@ export default function App() {
 		});
 	}
 
+	return (
+		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+			<View>
+				<Button title="Read" onPress={fetchBottles} />
+			</View>
+		</View>
+	);
+}
+
+function SettingsScreen() {
 	const createBottle = async () => {
 		const url = `http://10.0.2.2:8080/bottles/add`;
 		const response = await fetch(url);   // fetch page
@@ -23,18 +35,45 @@ export default function App() {
 	}
 
 	return (
-		<View style={styles.container}>
-			<Button title="Create" onPress={createBottle} />
-			<Button title="Read" onPress={fetchBottles} />
+		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+			<View>
+				<Button title="Create" onPress={createBottle} />
+			</View>
 		</View>
 	);
 }
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-});
+function ThirdScreen() {
+	return (
+		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+			<View>
+				<Button title="Third Screen" />
+			</View>
+		</View>
+	);
+}
+
+function FourthScreen() {
+	return (
+		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+			<View>
+				<Button title="Fourth Screen" />
+			</View>
+		</View>
+	);
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+	return (
+		<NavigationContainer>
+			<Tab.Navigator>
+				<Tab.Screen name="Home" component={HomeScreen} />
+				<Tab.Screen name="Settings" component={SettingsScreen} />
+				<Tab.Screen name="Third" component={ThirdScreen} />
+				<Tab.Screen name="Fourth" component={FourthScreen} />
+			</Tab.Navigator>
+		</NavigationContainer>
+	);
+}
