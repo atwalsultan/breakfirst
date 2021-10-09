63a929da577d7ca176f02a59080a4ef4048b5342
Sultan Atwal <sultan.singh.atwal@gmail.com>
2021-10-09 11:18:08 -0700

    #11 chore: improve directory structure

---
+++ client/components/screens/LibraryScreen.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native'
+
+const LibraryScreen = () => {
+    return (
+        <View>
+            <Text>Library Screen</Text>
+        </View>
+    )
+}
+
+export default LibraryScreen
+
+const styles = StyleSheet.create({})
