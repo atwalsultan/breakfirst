f4c32e1bb86f4ab0192d64a6a666ef76765615e5
Sultan Singh Atwal <58763741+atwalsultan@users.noreply.github.com>
2021-10-09 23:11:28 -0700

    #11 chore: improve directory structure (#12)

--- client/App.js
+++ client/App.js
@@ -1,22 +1,26 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { HomeScreen } from "./Screens/Home";
-import { StatisticsScreen } from "./Screens/Statistics";
-import { ReminderScreen } from "./Screens/Reminder";
-import { ProfileScreen } from "./Screens/Profile";
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+
+import HomeScreen from './components/screens/HomeScreen';
+import LibraryScreen from './components/screens/LibraryScreen';
+import StatsScreen from './components/screens/StatsScreen';
+import ProfileScreen from './components/screens/ProfileScreen';
 
 const Tab = createBottomTabNavigator();
+// const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Statistics" component={StatisticsScreen} />
-        <Tab.Screen name="Reminder" component={ReminderScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+	return (
+		<NavigationContainer>
+			<Tab.Navigator>
+				<Tab.Screen name="Home" component={HomeScreen} />
+				<Tab.Screen name="Library" component={LibraryScreen} />
+				<Tab.Screen name="Stats" component={StatsScreen} />
+				<Tab.Screen name="Profile" component={ProfileScreen} />
+			</Tab.Navigator>
+		</NavigationContainer>
+	);
 }
---
+++ client/components/screens/StatsScreen.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native'
+
+const StatsScreen = () => {
+    return (
+        <View>
+            <Text>Stats Screen</Text>
+        </View>
+    )
+}
+
+export default StatsScreen
+
+const styles = StyleSheet.create({})
