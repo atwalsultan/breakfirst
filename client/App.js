bec7263377298c846741a5c0276a4b04d858e88e
Sultan Atwal <sultan.singh.atwal@gmail.com>
2021-10-12 07:33:44 -0700

    #19 style: login and signup screens

--- client/App.js
+++ client/App.js
@@ -3,6 +3,7 @@ import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { NativeBaseProvider } from 'native-base';
 
 import HomeScreen from './components/screens/HomeScreen';
 import LibraryScreen from './components/screens/LibraryScreen';
@@ -15,15 +16,17 @@ const Tab = createBottomTabNavigator();
 
 export default function App() {
 	return (
-		<NavigationContainer>
-			{/* <Tab.Navigator>
-				<Tab.Screen name="Home" component={HomeScreen} />
-				<Tab.Screen name="Library" component={LibraryScreen} />
-				<Tab.Screen name="Stats" component={StatsScreen} />
-				<Tab.Screen name="Profile" component={ProfileScreen} />
-			</Tab.Navigator> */}
+		<NativeBaseProvider>
+			<NavigationContainer>
+				{/* <Tab.Navigator>
+					<Tab.Screen name="Home" component={HomeScreen} />
+					<Tab.Screen name="Library" component={LibraryScreen} />
+					<Tab.Screen name="Stats" component={StatsScreen} />
+					<Tab.Screen name="Profile" component={ProfileScreen} />
+				</Tab.Navigator> */}
 
-			<AuthenticationStack />
-		</NavigationContainer>
+				<AuthenticationStack />
+			</NavigationContainer>
+		</NativeBaseProvider>
 	);
 }
--- client/components/screens/LoginScreen.js
+++ client/components/screens/LoginScreen.js
@@ -1,22 +1,100 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { Box } from 'native-base';
 
 const LoginScreen = ({ navigation }) => {
     return (
-        <View>
-            <Text>Login Screen</Text>
-            <Button title="Login" onPress={() => {
-                navigation.replace("Onboarding");
-                navigation.navigate("Onboarding");
-            }} />
-            <Button title="Signup" onPress={() => {
-                navigation.replace("Signup");
-                navigation.navigate("Signup");
-            }} />
-        </View>
+        <Box style={styles.container} safeAreaTop>
+            <Box style={styles.image}></Box>
+
+            <Text style={styles.h1}>Log In</Text>
+            <Text style={styles.h2}>Don't forget to take a break!</Text>
+            <TextInput placeholder="Email" style={styles.input} />
+            <TextInput placeholder="Password" style={styles.input} />
+            <Text style={styles.forgotPassword}>Forgot password?</Text>
+
+            <TouchableOpacity>
+                <Text style={styles.loginButton} onPress={() => {
+                    navigation.replace("Onboarding");
+                    navigation.navigate("Onboarding");
+                }}>
+                    Login
+                </Text>
+            </TouchableOpacity>
+
+            <View style={styles.signupInfo}>
+                <Text style={styles.signupText}>Do not have an account?</Text>
+                <Text style={styles.signupLink} onPress={() => {
+                    // Logic to log in
+
+                    // So that user cannot get back to this page once navigated to the onboarding stack
+                    navigation.replace("Signup");
+                    navigation.navigate("Signup");
+                }}>
+                    Signup here.
+                </Text>
+            </View>
+        </Box>
     )
 }
 
 export default LoginScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        paddingHorizontal: 16,
+        paddingBottom: 42,
+        paddingTop: 75,
+        flex: 1,
+    },
+    image: {
+        height: '25%',
+        backgroundColor: 'rgba(0,0,0,0.05)',
+        marginBottom: 32,
+    },
+    h1: {
+        fontSize: 30,
+        fontWeight: "700",
+        textAlign: "center",
+        marginBottom: 8
+    },
+    h2: {
+        fontSize: 18,
+        fontWeight: "400",
+        textAlign: "center",
+        marginBottom: 32
+    },
+    input: {
+        padding: 16,
+        backgroundColor: 'rgba(0,0,0,0.05)',
+        fontSize: 18,
+        marginBottom: 16
+    },
+    forgotPassword: {
+        fontSize: 16,
+        fontWeight: "400",
+        textAlign: "right",
+        marginBottom: 32,
+    },
+    loginButton: {
+        width: "45%",
+        backgroundColor: "rgba(0,0,0,0.1)",
+        paddingVertical: 16,
+        textAlign: 'center',
+        alignSelf: "center"
+    },
+    signupInfo: {
+        flexGrow: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'flex-end'
+    },
+    signupText: {
+        fontSize: 16,
+    },
+    signupLink: {
+        fontWeight: '700',
+        fontSize: 16,
+        marginLeft: 2
+    }
+})
