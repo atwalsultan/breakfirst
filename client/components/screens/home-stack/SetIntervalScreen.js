884c0663b166021a98daac1e36d162804144a335
Sultan Singh Atwal <58763741+atwalsultan@users.noreply.github.com>
2021-10-20 10:19:51 -0700

    #31 style: set interval & change schedule screens (wireframes) (#32)

--- client/components/screens/home-stack/SetIntervalScreen.js
+++ client/components/screens/home-stack/SetIntervalScreen.js
@@ -1,17 +1,83 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Center } from 'native-base';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { Box } from 'native-base';
 
-const SetIntervalScreen = () => {
+const SetIntervalScreen = ({ navigation }) => {
     return (
-        <View>
-            <Center>
-                <Text>Set Interval</Text>
-            </Center>
-        </View>
+        <Box style={styles.container} safeAreaTop>
+            <Box style={styles.header}>
+                <Text style={styles.backLink} onPress={() => {
+                    navigation.goBack();
+                }}>
+                    Back
+                </Text>
+                <Text style={styles.heading}>Interval</Text>
+            </Box>
+
+            <TouchableOpacity>
+                <Box style={styles.card}>
+                    <Text style={styles.cardTitle}>30 mins</Text>
+                </Box>
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+                <Box style={styles.card}>
+                    <Text style={styles.cardTitle}>45 mins</Text>
+                </Box>
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+                <Box style={styles.card}>
+                    <Text style={styles.cardTitle}>60 mins</Text>
+                </Box>
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+                <Box style={styles.card}>
+                    <Text style={styles.cardTitle}>Decide for me</Text>
+                </Box>
+            </TouchableOpacity>
+        </Box>
     )
 }
 
 export default SetIntervalScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        paddingHorizontal: 16,
+        paddingBottom: 24,
+        paddingTop: 34,
+        flex: 1,
+    },
+    header: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 24
+    },
+    backLink: {
+        fontSize: 18,
+        fontWeight: '400',
+        position: 'absolute',
+        left: 0,
+    },
+    heading: {
+        fontSize: 20,
+        fontWeight: '600',
+    },
+    card: {
+        paddingHorizontal: 16,
+        paddingBottom: 10,
+        paddingTop: 13,
+        backgroundColor: 'rgba(0,0,0,0.05)',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginTop: 16
+    },
+    cardTitle: {
+        fontSize: 18,
+        fontWeight: '400',
+    },
+})
