c05edc61919fce6461fe776a2f18b2523abf94e5
Sultan Singh Atwal <58763741+atwalsultan@users.noreply.github.com>
2021-10-22 10:23:11 -0700

    #41 style: library stack (wireframes) (#44)

--- client/components/screens/library-stack/ExerciseDetailsScreen.js
+++ client/components/screens/library-stack/ExerciseDetailsScreen.js
@@ -1,17 +1,69 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Center } from 'native-base';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { Box } from 'native-base';
+import { alignSelf } from 'styled-system';
 
 const ExerciseDetailsScreen = ({ navigation }) => {
     return (
-        <View>
-            <Center>
-                <Text>ExerciseDetailsScreen</Text>
-            </Center>
-        </View>
+        <Box style={styles.container} safeAreaTop>
+            <TouchableOpacity onPress={() => {
+                navigation.goBack();
+            }}>
+                <Text style={styles.backLink}>Back</Text>
+            </TouchableOpacity>
+
+            <Box style={styles.exerciseImage}></Box>
+
+            <Text style={styles.exerciseTitle}>Save the neck</Text>
+            <Text style={styles.exerciseDescription}>Please follow the cat to relax your neck and back. Click the button below to add this exercise to your collection.</Text>
+
+            <TouchableOpacity style={styles.saveButton} onPress={() => {
+                navigation.goBack();
+            }}>
+                <Text style={styles.saveButtonText}>Add to collection</Text>
+            </TouchableOpacity>
+        </Box>
     )
 }
 
 export default ExerciseDetailsScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        paddingHorizontal: 16,
+        paddingTop: 34,
+        flex: 1,
+    },
+    backLink: {
+        fontSize: 18,
+        fontWeight: '400',
+        marginBottom: 16
+    },
+    exerciseImage: {
+        height: '50%',
+        backgroundColor: 'rgba(0,0,0,0.06)',
+        marginBottom: 24,
+    },
+    exerciseTitle: {
+        alignSelf: 'center',
+        fontSize: 20,
+        fontWeight: '700',
+        marginBottom: 16
+    },
+    exerciseDescription: {
+        fontSize: 16,
+        fontWeight: '400',
+        marginBottom: 58
+    },
+    saveButton: {
+        backgroundColor: 'rgba(0,0,0,0.1)',
+        padding: 16,
+        minWidth: '50%',
+        alignSelf: 'center',
+    },
+    saveButtonText: {
+        fontSize: 18,
+        fontWeight: '400',
+        textAlign: 'center',
+    }
+})
--- client/components/screens/library-stack/FactsScreen.js
+++ client/components/screens/library-stack/FactsScreen.js
@@ -1,17 +1,96 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Center } from 'native-base';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { Box } from 'native-base';
 
 const FactsScreen = ({ navigation }) => {
     return (
-        <View>
-            <Center>
-                <Text>FactsScreen</Text>
-            </Center>
-        </View>
+        <Box style={styles.container} safeAreaTop>
+            <TouchableOpacity onPress={() => {
+                navigation.goBack();
+            }}>
+                <Text style={styles.backLink}>Back</Text>
+            </TouchableOpacity>
+
+            <Box style={styles.tabs}>
+                <Box style={[styles.tab, { backgroundColor: 'rgba(0,0,0,0.1)' }]}>
+                    <Text style={styles.tabText}>Facts</Text>
+                </Box>
+                <Box style={[styles.tab, { backgroundColor: 'rgba(0,0,0,0.0)' }]}>
+                    <Text style={styles.tabText}>Prevention</Text>
+                </Box>
+            </Box>
+
+            <Box style={styles.factsList}>
+                <Box style={styles.fact}>
+                    <Text style={styles.factType}>Fact of the day</Text>
+                    <Text style={styles.factTitle}>Effects of a sedentary life</Text>
+                    <Text style={styles.factText}>Sedentary lifestyles increase all causes of mortality, double the risk of cardiovascular diseases, diabetes, and obesity, and increase the risks of colon cancer, high blood pressure, osteoporosis, lipid disorders, depression and anxiety.</Text>
+                </Box>
+                <Box style={styles.fact}>
+                    <Text style={styles.factType}>Fact of the day</Text>
+                    <Text style={styles.factTitle}>Effects of a sedentary life</Text>
+                </Box>
+                <Box style={styles.fact}>
+                    <Text style={styles.factType}>Fact of the day</Text>
+                    <Text style={styles.factTitle}>Research and guidelines</Text>
+                </Box>
+            </Box>
+        </Box>
     )
 }
 
 export default FactsScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        paddingHorizontal: 16,
+        paddingTop: 34,
+        flex: 1,
+    },
+    backLink: {
+        fontSize: 18,
+        fontWeight: '400',
+        marginBottom: 16
+    },
+    tabs: {
+        flexDirection: 'row',
+        marginBottom: 16,
+        padding: 8,
+        backgroundColor: 'rgba(0,0,0,0.06)'
+    },
+    tab: {
+        width: '50%',
+        paddingVertical: 12,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    tabText: {
+        fontSize: 18,
+        fontWeight: '400'
+    },
+    factsList: {
+
+    },
+    fact: {
+        backgroundColor: 'rgba(0,0,0,0.06)',
+        marginBottom: 16,
+        padding: 16,
+        paddingBottom: 0
+    },
+    factType: {
+        fontSize: 14,
+        fontWeight: '400',
+        color: 'rgba(0,0,0,0.68)',
+        marginBottom: 8
+    },
+    factTitle: {
+        fontSize: 22,
+        fontWeight: '400',
+        marginBottom: 16
+    },
+    factText: {
+        fontSize: 16,
+        fontWeight: '400',
+        marginBottom: 16
+    }
+})
